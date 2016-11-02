bbac2e478bb74e788d98ad44f0b00a6e9b903be7
maydev99 <may@droidloft.com>
2016-11-02 16:02:12 -0400

    test

---
+++ app/src/main/java/com/droidloft/newapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.droidloft.newapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * TestTestTestTestTestTestTest
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
